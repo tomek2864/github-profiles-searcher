9a865a3dcfa644341c06bd9c4222e733dfc8e740
Tomek <tomek2864@gmail.com>
2019-06-07 23:59:29 +0200

    Search bar done

---
+++ src/components/layout/SearchBar.jsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+const SearchBar = () => {
+  return (
+    <Fragment>
+      <input />
+      <button>Search</button>
+    </Fragment>
+  );
+};
+
+export default SearchBar;
